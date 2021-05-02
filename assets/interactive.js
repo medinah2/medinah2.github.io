3039a326e7542fbbfa7fd08f23f9b5e6ada6a5a9
medinah2 <medinah2@tcnj.edu>
2021-05-01 20:02:29 -0400

    Updated pages

--- assets/interactive.js
+++ assets/interactive.js
@@ -1,4 +1,14 @@
-function dropDownMenu(){
-	document.getElementById('text').style.fontSize = "24pt";
-	console.log("huh");
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function dropDownMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
